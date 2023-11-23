be7da473a5c9c4b29b1d567552e1c21c52147ad1
Kishor Muruganandham <kmuruganandham@fssa.freshworks.com>
2023-11-23 08:50:08 +0530

    Fix Issues

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,11 +27,11 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
+                        alt={`${invoice.name}'s profile picture`}
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
